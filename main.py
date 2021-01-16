0d0bfa08b871c0867c52f1845674ba0ff49f6ebc
Jos <vuhuynh9x>
2021-01-16 16:42:28 +0700

    add print test fetch  2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print("hello world")
     print("hello chi")
     print("hello huynh")
-    print("test git fetch 1")
+    print("test git fetch 2")
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
